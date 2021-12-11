1c79f3ec2dd1874a5136d2df0c3fe999c73ef97d
Hendrik-Alberts <75767113+Hendrik-Alberts@users.noreply.github.com>
2021-12-11 15:21:06 -0700

    Various Changes

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ textInput.value = "";
 
 addBtn.addEventListener("click", function () {
   const toDoOne = textInput.value;
-  //Function to create LI element
-  const listItem = function () {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(toDoOne));
-    ul.appendChild(li);
-  };
-  listItem(); //Do I need this call here or can it look better?
 
-  textInput.value = "";
-  console.log(toDoOne);
+  if (toDoOne != "") {
+    //Function to create LI element
+    const listItem = function () {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(toDoOne));
+      ul.appendChild(li);
+
+      //This creates the 'x' to the right of each task.
+      const del = document.createElement("button");
+      del.innerHTML = "DONE";
+      li.appendChild(del);
+      del.setAttribute("id", "xout");
+      del.addEventListener("click", function () {
+        li.setAttribute("hidden", true);
+      });
+    };
+    listItem(); //Do I need this call here or can it look better?
+  } else {
+    alert("Please add a task!");
+  }
 });
 
 //Code to clear text input field on click
@@ -25,10 +36,9 @@ clearBtn.addEventListener("click", function () {
   textInput.value = "";
 });
 
-// Trigger "Click to Add" on enter push
-
-//Create 'x' or delete buttons on all tasks?
-
-//Aternate background color of tasks?
-
-//Make the task thumbnail look better.
+//Note: Trigger "Click to Add" on enter push. I researched this forever on MDN since keyCode is deprecated. By using the KeyboardEvent.key property I only need to enter the string of the corresponding key as per the specs. This allows for more devices to be able to use this, as opposed to just using the number system on US Keyboards. Yes, there are other nations out there with better keyboard layouts and I intend to make life easier for then too.  :)
+textInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    addBtn.click();
+  }
+});
